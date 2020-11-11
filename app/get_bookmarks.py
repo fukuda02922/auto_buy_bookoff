514ed055078f58defb8aa3a733055c4f0a1f6c00
fukuda <fukuda@liveworks.co.jp>
2020-11-11 15:02:14 +0900

    bookmark検索

--- app/get_bookmarks.py
+++ app/get_bookmarks.py
@@ -1,64 +1,60 @@
 from auto_buy_log import Log
-
-import time
 from datetime import datetime
-import sys
-import traceback
-import requests
-import json
-import csv
+import sys, os, traceback, requests, json, csv, time, pathlib, random
 from threading import Thread, Lock
 
-TH_COUNT = 10 # スレッド数
+TH_COUNT = 100 # スレッド数
 MAIN_URL = 'https://www.bookoffonline.co.jp'
-LIMIT = 100
+LIMIT = 100000
 PROCESS_TIME = 60 * 60 * 1
-session = requests.Session()
 count = 0
 start_time = time.time()
-iscd_list = {}
+memNo_50over = []
 lock = Lock()
+now = datetime.now()
+random_list = list(range(8000000, 11000000))
 
+filename = os.path.dirname(__file__) + '/bookmark/{}_{}_{}.csv'.format(now.year, now.month, now.day)
+filename_over50 = os.path.dirname(__file__) + '/bookmark/{}_{}_{}.csv'.format(now.year, now.month, now.day)
+if os.path.exists(filename):
+    os.remove(filename)
+pathlib.Path(filename)
 
 def next_memNo():
     global count
     count += 1
-    return 8000000 + count
+    return random.choice(random_list)
 
 def run():
-    global iscd_list
+    global memNo_50over
     while True:
-        memNo = next_memNo()
-        if LIMIT < count or start_time <:
+        print(count)
+        if LIMIT < count:
             return
         if (time.time() - start_time) > PROCESS_TIME:
             return
         try:
-            response = session.get(MAIN_URL + '/spf-api2/goods_souko/bookmark/' + memNo)
+            print(memNo)
+            response = requests.get(MAIN_URL + '/spf-api2/goods_souko/bookmark/{}'.format(memNo))
+            star_list_json = json.loads(response.content)
+            if star_list_json['rcptList']:
+                for star in star_list_json['rcptList']:
+                    if star['used_warehouse'] == 0:
+                        lock.acquire()
+                        with open(filename, 'a') as f:
+                            w = csv.writer(f)
+                            w.writerow([star['instorecode'], 1])
+                        lock.release()
+                if star_list_json['rcptList'].length >= 50:
+                    memNo_50over.append(memNo)
         except Exception:
             pass
-        star_list_json = json.loads(response.content)
-        if star_list_json:
-            lock.acquire()
-            for star in star_list_json['rcptList']:
-                if not (star['instorecode'] in iscd_list.keys()):
-                    iscd_list[star['instorecode']] = 1
-                else:
-                    iscd_list[star['instorecode']] += 1
-            lock.release()
 
-th_pool = [Thread(target=run) for index in range(TH_COUNT)]
-
-for th in th_pool:
-    th.start()
-
-for th in th_pool:
-    th.join()
-
-filename = os.path.dirname(__file__) + '/bookmark/{}_{}_{}.csv'.format(now.year, now.month, now.day)
-if not os.path.exists(self.filename):
-    pathlib.Path(self.filename)
-
-with open(filename, 'w') as f:
-    w = csv.DictWriter(f, iscd_list.keys())
-    w.writerow(iscd_list)
+try:
+    th_pool = [Thread(target=run) for index in range(TH_COUNT)]
+    for th in th_pool:
+        th.start()
+    for th in th_pool:
+        th.join()
+except KeyboardInterrupt:
+    print(memNo_50over)
